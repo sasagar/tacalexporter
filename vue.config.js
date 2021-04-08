2264d6c400193b38f4505e2ad60f7712771ebfb1
SASAGAWA Kiyoshi <sasagawa@kent-and-co.com>
2021-04-08 12:09:00 +0900

    test for dmg.

--- vue.config.js
+++ vue.config.js
@@ -73,9 +73,6 @@ module.exports = {
           gatekeeperAssess: false,
           entitlements: "build/entitlements.mac.plist",
           entitlementsInherit: "build/entitlements.mac.plist",
-          dmg: {
-            sign: false,
-          },
         },
       },
     },
